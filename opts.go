ade8bbfbd152ac70630994005844fab56a84b322
Titus Tienaah <titus.intdxdt@gmail.com>
2018-06-05 05:30:22 -0300

    json

--- opts.go
+++ opts.go
@@ -2,13 +2,13 @@ package opts
 
 //Opts
 type Opts struct {
-	Threshold              float64
-	MinDist                float64
-	RelaxDist              float64
-	PlanarSelf             bool
-	NonPlanarSelf          bool
-	AvoidNewSelfIntersects bool
-	GeomRelation           bool
-	DistRelation           bool
-	DirRelation            bool
+	Threshold              float64  `json:"threshold"`
+	MinDist                float64  `json:"mindist"`
+	RelaxDist              float64  `json:"relaxdist"`
+	PlanarSelf             bool     `json:"planarself"`
+	NonPlanarSelf          bool     `json:"nonplanarself"`
+	AvoidNewSelfIntersects bool     `json:"avoidself"`
+	GeomRelation           bool     `json:"geomrelate"`
+	DistRelation           bool     `json:"distrelate"`
+	DirRelation            bool     `json:"dirrelate"`
 }
